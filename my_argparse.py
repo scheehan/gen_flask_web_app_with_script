d351f6743d46a9c6f827d6b6138a1c4b64266aba
scheehan <siowcheehan@gmail.com>
2025-02-09 09:24:25 +0800

    insert default path

--- my_argparse.py
+++ my_argparse.py
@@ -1,17 +1,20 @@
 import argparse
 import os, sys, re
-import MyVar
+from MyVar import myclassvar
 
 from platform import python_version
 from pathlib import Path
 
+basedir = os.path.abspath(os.path.dirname(__name__))
+
 parser = argparse.ArgumentParser(description='create python flask web app and others')
+parser.add_argument('-p', '--path', dest='path', metavar='path name', action='store', default=Path(basedir).parents[0])
 parser.add_argument(dest='foldername', metavar='foldername', nargs='?', action='store')
-parser.add_argument('-p', '--progr', metavar='app name', dest='progr', action='store', default='flask')
+parser.add_argument('-a', '--app', metavar='app name', dest='progr', action='store', default='flask')
 
 args = parser.parse_args()
 myfoldername = args.foldername
-basedir = os.path.abspath(os.path.dirname(__name__))
+mypath = args.path
 
 toplistoffiles = ['LICENSE', 'README.md', 'config.py', ]
 sublistoffiles = ['__init__.py', 'routes.py', 'db.py', 'schema.sql']
@@ -21,7 +24,7 @@ def main():
     try:
         # filename most be alphanumeric characters and underscores only
         if re.match(r'[A-Za-z0-9_-]+$', myfoldername):
-            Path(myfoldername).mkdir()
+            Path(mypath, myfoldername).mkdir()
             Path(os.path.join(basedir, myfoldername, 'app')).mkdir()
         else:
             print('''
@@ -38,11 +41,11 @@ def main():
         
         with open(os.path.join(basedir, myfoldername, i ), 'w') as f:
             if i == 'LICENSE':
-                f.write(MyVar.licensecontent)
+                f.write(myclassvar.licensecontent)
             if i == 'README.md':
-                f.write(MyVar.myreadmenote)
+                f.write(myclassvar.myreadmenote)
             if i == 'config.py':
-                f.write(MyVar.flaskconfig)
+                f.write(myclassvar.flaskconfig)
 
 
     for j in subfoldernames:
@@ -50,20 +53,20 @@ def main():
         for k in sublistoffiles:
             with open(os.path.join(basedir, myfoldername, 'app', k ), 'w') as f:
                 if k == '__init__.py':
-                    f.write(MyVar.myinitcontent)
+                    f.write(myclassvar.myinitcontent)
                 if k == 'routes.py':
-                    f.write(MyVar.myroutesconfig)
+                    f.write(myclassvar.myroutesconfig)
                 if k == 'db.py':
-                    f.write(MyVar.mydbconfig)
+                    f.write(myclassvar.mydbconfig)
                 if k == 'schema.sql':
-                    f.write(MyVar.dbschemasetup)
+                    f.write(myclassvar.dbschemasetup)
         if j == 'templates':
             with open(os.path.join(basedir, myfoldername, 'app', j, 'index.html'), 'w') as f:
-                f.write(MyVar.myhtmlindex)
+                f.write(myclassvar.myhtmlindex)
         if j == 'static':
             os.mkdir(os.path.join(basedir, myfoldername, 'app', j, 'css'))
             with open(os.path.join(basedir, myfoldername, 'app', j, 'css', 'main.css'), 'w') as f:
-                f.write(MyVar.mycsscontent)
+                f.write(myclassvar.mycsscontent)
             
 if __name__ == '__main__':
     main()
