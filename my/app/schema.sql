2903b7fe48439916b10453a18f148093b6549dac
scheehan <siowcheehan@gmail.com>
2025-02-09 12:59:25 +0800

    remove

--- my/app/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS users;
-
-    CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE NOT NULL,
-    ext TEXT NOT NULL,
-    email TEXT
-    );
-    
